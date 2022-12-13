7b20ca58f00ea731a8ee171e1a1903cb0c4d7e2d
lostgrammer <carlos_cmvp@hotmail.com>
2022-12-13 10:39:34 -0500

    (arreglado)agregando serializados no basados en modelos

--- taller/settings.py
+++ taller/settings.py
@@ -39,11 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'todos',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    #agregado
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,3 +126,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+  "https://mywebsite.com",
+  "https://blog.mywebsite.com",
+  "http://localhost:8080",
+  "http://127.0.0.1:9000",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
--- todos/api.py
+++ todos/api.py
@@ -1,9 +1,14 @@
 from .models import Todo
 from rest_framework import viewsets, permissions
-from .serializer import TodoSerializer
+from .serializer import TodoSerializer, TestTodoSerializer
 
 class TodoViewSet(viewsets.ModelViewSet): #viewsets: indicamos la clase serializada
     queryset = Todo.objects.all()
     permission_classes = [permissions.AllowAny]
     #indicar el serializer que usa esos datos
     serializer_class = TodoSerializer
+
+class TestViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = TestTodoSerializer
