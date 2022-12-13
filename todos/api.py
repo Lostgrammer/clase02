0d4a3b7efc0d50d8d872c7cbe0355b7b67a8051d
lostgrammer <carlos_cmvp@hotmail.com>
2022-12-13 10:53:35 -0500

    arreglando merge todos/api

--- todos/api.py
+++ todos/api.py
@@ -11,8 +11,4 @@ class TodoViewSet(viewsets.ModelViewSet): #viewsets: indicamos la clase serializ
 class TestViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     permission_classes = [permissions.AllowAny]
-<<<<<<< HEAD
     serializer_class = TestTodoSerializer
-=======
-    serializer_class = TestTodoSerializer
->>>>>>> neorama
