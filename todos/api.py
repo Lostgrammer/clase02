048c4e4eb838486b154886affc261a1092ccdec9
lostgrammer <carlos_cmvp@hotmail.com>
2022-12-14 10:48:09 -0500

    delete no funciona

--- todos/api.py
+++ todos/api.py
@@ -1,6 +1,10 @@
 from .models import Todo
 from rest_framework import viewsets, permissions
 from .serializer import TodoSerializer, TestTodoSerializer
+#nuevos
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
 class TodoViewSet(viewsets.ModelViewSet): #viewsets: indicamos la clase serializada
     queryset = Todo.objects.all()
@@ -12,3 +16,10 @@ class TestViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = TestTodoSerializer
+
+class DeleteAllTodo(APIView):
+    def delete(self, request):
+        # Elimina todos los registros
+        Todo.objects.all().delete()
+        # Retorna un status code de 204 indicando que no existe contenido dentro de nuestra base de datos
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- todos/urls.py
+++ todos/urls.py
@@ -1,6 +1,6 @@
 #simula el get y el post
 from rest_framework import routers
-from .api import TodoViewSet, TestViewSet
+from .api import TodoViewSet, TestViewSet,DeleteAllTodo
 from django.urls import path
 
 router = routers.DefaultRouter()
@@ -10,5 +10,11 @@ router.register('api/todos', TodoViewSet, 'todos') #creando url de mi api "local
 #test
 router.register('api/test', TestViewSet, 'test')
 
-urlpatterns = router.urls #todas la rutas
+urlpatterns = router.urls
+
+urlpatterns += [path('api/todos/deleteAll',DeleteAllTodo.as_view(), name='deleteAll'),]
+
+#urlpatterns += router.urls
+
+
 
