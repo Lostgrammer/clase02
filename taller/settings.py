5f2fb13385c3e4fc6c92fc42757496812d098195
lostgrammer <carlos_cmvp@hotmail.com>
2022-12-17 20:13:20 -0500

    clase

--- taller/settings.py
+++ taller/settings.py
@@ -145,7 +145,16 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE':10
+    'PAGE_SIZE':10,
+    'DEFAULT_THROTTLE_CLASSES': [
+            'rest_framework.throttling.AnonRateThrottle',
+            'rest_framework.throttling.UserRateThrottle'
+        ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
+
 }
 
 AUTH_USER_MODEL = 'users.User'
